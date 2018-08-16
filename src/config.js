1d478ecef11b12c74a6385ce4a66916291b49b6d
swwapnilk24 <35681229+swwapnilk24@users.noreply.github.com>
2018-08-16 11:21:35 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -39,9 +39,9 @@ const config = {
   },
   test: {
     mongo: {
-      //uri: 'mongodb://localhost/spmapi-test',
+        uri: 'mongodb://localhost/spmapi-test',
       //uri: 'mongodb://172.17.0.1/spmapi-test',
-        uri: 'mongodb://172.19.0.2:27017/spmapi-test',
+      //uri: 'mongodb://172.19.0.2:27017/spmapi-test',
       options: {
         debug: false
       }
@@ -49,9 +49,9 @@ const config = {
   },
   development: {
     mongo: {
-        //uri: 'mongodb://172.17.0.1/spmapi-dev',
+          uri: 'mongodb://172.17.0.1/spmapi-dev',
         //uri: 'mongodb://localhost/spmapi-dev',
-          uri: 'mongodb://172.19.0.2:27017/spmapi-dev',
+        //uri: 'mongodb://172.19.0.2:27017/spmapi-dev',
       options: {
         debug: true
       }
@@ -62,8 +62,8 @@ const config = {
     port: process.env.PORT || 8080,
     mongo: {
       //uri: process.env.MONGODB_URI || 'mongodb://172.17.0.1/spmapi'
-      //uri: process.env.MONGODB_URI || 'mongodb://localhost/spmapi'
-      uri: process.env.MONGODB_URI || 'mongodb://172.19.0.2:27017/spmapi'
+        uri: process.env.MONGODB_URI || 'mongodb://localhost/spmapi'
+      //uri: process.env.MONGODB_URI || 'mongodb://172.19.0.2:27017/spmapi'
     }
   }
 }
